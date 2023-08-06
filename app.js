d1cb66f8c2e4f1265f6c1550c058e41926386318
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-08-06 20:33:31 +0800

    finished calculator but doesn't reset when pushing another button that isn't the equals sign

---
+++ app.js
@@ -0,0 +1,18 @@
+const buttons = document.getElementsByClassName("button");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    const clickedButton = buttons[i].textContent;
+    document.getElementById("screen").textContent += clickedButton;
+  });
+}
+
+const equalsButton = document.getElementById("equals-button")
+
+equalsButton.addEventListener("click", ()=> {
+  let screenContent = document.getElementById("screen").textContent
+  const screen = document.getElementById("screen");
+  screen.textContent = eval(screenContent);
+  
+  
+})
